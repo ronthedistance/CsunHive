b6d3d4becf3d6a7f14ad4490d894619c9557eb79
ronthedistance <ron.revilla.30@my.csun.edu>
2019-11-10 13:53:55 -0800

    Update mainUbuntu.sh

--- sc/mainUbuntu.sh
+++ sc/mainUbuntu.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 ${cat /etc/*release}
 
+#this enables the firewall
 sudo ufw enable
 
+#prevent the use of guest logins via LightDM user management
 echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 
+#edits password configu files , 90 days maximum, 10 days minimum, with a login warning of 1 week
 sudo sed -i '/^PASS_MAX_DAYS/ c\PASS_MAX_DAYS   90' /etc/login.defs
 sudo sed -i '/^PASS_MIN_DAYS/ c\PASS_MIN_DAYS   10'  /etc/login.defs
 sudo sed -i '/^PASS_WARN_AGE/ c\PASS_WARN_AGE   7' /etc/login.defs
 
+#Asks if you want to install vsftpd and configure it. Else: removes it.
 echo -n "VSFTP [Y/n] "
 read option
 if [[ $option =~ ^[Yy]$ ]]
@@ -24,12 +28,13 @@ else
   sudo apt-get -y purge vsftpd*
 fi
 
-
+#finds media files with these suffixes and removes them
 for suffix in mp3 txt wav wma aac mp4 mov avi gif jpg png bmp img exe msi bat sh
 do
   sudo find /home -name *.$suffix
 done
 
+#removes what are typically referred to as hacking tools within the competition
 sudo apt-get -y nmap
 sudo apt-get -y wireshark
 sudo apt-get -y zenmap
