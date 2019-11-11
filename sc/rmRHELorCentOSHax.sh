6ca0a456270e7a6aad4d82c849a693a969bcc53a
ronthedistance <ron.revilla.30@my.csun.edu>
2019-11-11 10:17:23 -0800

    Update rmRHELorCentOSHax.sh

--- sc/rmRHELorCentOSHax.sh
+++ sc/rmRHELorCentOSHax.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ${cat /etc/*release}
-
-yum remove nmap
-
-yum remove wireshark
-
-yum remove zenmap
-
+yum remove -y nmap*
+yum remove -y wireshark*
+yum remove -y zenmap*
+yum remove-y nikto* 
+yum remove-y purge hydra*
+yum remove-y purge john*
+yum remove-y purge nikto*
+yum remove -y purge netcat*
+yum remove -y purge ophcrack*
